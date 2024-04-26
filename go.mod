098c08f02944eb5aa15f21ed41bdc531833a675e
rawanmahdi <mahdir3@mcmaster.ca>
2024-04-26 09:36:47 -0400

    Go project setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rawanmahdi/docker-but-worse
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	default:
+		panic("please enter a correct command")
+	}
+}
+
+func run() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// cmd.SysProcAttr = &syscall.SysProcAttr {
+	// 	Cloneflags: syscall.CLONE_NEWUTS,
+
+	// }
+	cmd.Run()
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
