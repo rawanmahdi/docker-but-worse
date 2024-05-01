e23299673b9415e58143d16ff55541a44484fa70
rawanmahdi <mahdir3@mcmaster.ca>
2024-05-01 19:38:41 -0400

    Added logic to take in cmd args and generate hostname

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("please enter a correct command")
 	}
@@ -18,17 +21,32 @@ func main() {
 func run() {
 	fmt.Printf("Running %v\n", os.Args[2:])
 
+	cmd := exec.Command("proc/self.exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr {
+		Cloneflags: syscall.CLONE_NEWUTS, 
+
+	}
+	cmd.Run()
+}
+
+
+func child() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+
+	syscall.Sethostname([]byte("container"))
+
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	// cmd.SysProcAttr = &syscall.SysProcAttr {
-	// 	Cloneflags: syscall.CLONE_NEWUTS,
-
-	// }
 	cmd.Run()
 }
+
 
 func must(err error) {
 	if err != nil {
